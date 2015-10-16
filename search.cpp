3f70da28ae3f715d38c3d7d692a2257285558b8a
prime <prime@kmc.gr.jp>
2015-10-16 15:00:46 +0900

    memorize search results

--- search.cpp
+++ search.cpp
@@ -3,15 +3,19 @@
 #include <unordered_map>
 #include <boost/optional.hpp>
 
-boost::optional<std::vector<pos>> dfs(const State &st, int rem, const Board &prev) {
+boost::optional<std::vector<pos>> dfs(const State &st, int rem,
+    const Board &prev, std::unordered_map<Board, int> &memo) {
   if (st.bd.is_goal()) return std::vector<pos>();
   if (rem < st.score) return boost::none;
+  auto itr = memo.find(st.bd);
+  if (itr != std::end(memo) && itr->second <= st.score) return boost::none;
+  memo[st.bd] = st.score;
   for (auto next : next_states(st)) {
     State nx;
     pos p;
     std::tie(nx, p) = next;
     if (nx.bd == prev) continue;
-    if (auto opt_ans = dfs(nx, rem, st.bd)) {
+    if (auto opt_ans = dfs(nx, rem, st.bd, memo)) {
       opt_ans->emplace_back(p);
       return *opt_ans;
     }
@@ -20,8 +24,9 @@ boost::optional<std::vector<pos>> dfs(const State &st, int rem, const Board &pre
 }
 std::vector<pos> find_answer(const State &st) {
   for (int i = 10; i <= 2000; i += 10) {
+    std::unordered_map<Board, int> memo;
     std::cerr << i << std::endl;
-    if (auto opt_ans = dfs(st, i, Board())) {
+    if (auto opt_ans = dfs(st, i, Board(), memo)) {
       std::reverse(std::begin(*opt_ans), std::end(*opt_ans));
       return *opt_ans;
     }
